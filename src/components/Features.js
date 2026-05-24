e5c2cd3edfae2b0e7e315146c114e2e9d6569880
iafrankov-star <i.a.frankov@yar.etagi.com>
2026-05-24 16:22:56 +0300

    advantages/features section

---
+++ src/components/Features.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Features.css';
+
+export default function Features() {
+  return (
+    <section className="features">
+      <div>Экономия времени и денег</div>
+      <div>Проверка и юридическая безопасность</div>
+      <div>Сопровождение сдел��к под ключ</div>
+      <div>Индивидуальный подход</div>
+    </section>
+  );
+}
