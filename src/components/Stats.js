4322bcb73921587878ce60dd66469fbe0967bdc3
iafrankov-star <i.a.frankov@yar.etagi.com>
2026-05-24 16:22:13 +0300

    stats section

---
+++ src/components/Stats.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Stats.css';
+
+export default function Stats() {
+  return (
+    <section className="stats">
+      <div>
+        <b>5+</b>
+        <span>опыт работы</span>
+      </div>
+      <div>
+        <b>300+</b>
+        <span>довольных клиентов</span>
+      </div>
+      <div>
+        <b>500+</b>
+        <span>успешных сделок</span>
+      </div>
+      <div>
+        <b>6 регионов</b>
+        <span>работы по всей России</span>
+      </div>
+    </section>
+  );
+}
